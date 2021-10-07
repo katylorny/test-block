ced8c4328c5e91fc010ad4144fce47e1482313de
katylorny <katy_lorny@mail.ru>
2021-10-07 12:33:10 +0300

    changing list width when the right edge is too close

--- script.js
+++ script.js
@@ -24,10 +24,14 @@ fetch(`https://jsonplaceholder.typicode.com/posts/`)
 
     })
 
+const BLOCK_TOP = 300
+const LIST_WIDTH = 250
+
 // создание элемента списка
 const content = document.querySelector(`.content`)
 const list = document.createElement(`ul`)
 list.classList.add(`list`, `hidden`)
+
 
 // скрытие-показ списка по клику на блок
 const block = document.querySelector(`.block`)
@@ -35,17 +39,24 @@ block.addEventListener(`click`, () => {
     list.classList.contains(`hidden`) ? list.classList.remove(`hidden`) : list.classList.add(`hidden`)
 })
 
-// расположение списка
+// расположение и ширина списка
 let blockPosition = block.getBoundingClientRect()
 let blockWidth = block.clientWidth
+
+const distanceToRight = content.offsetWidth - block.getBoundingClientRect().left - block.clientWidth
+list.style.width = `${distanceToRight > LIST_WIDTH ? LIST_WIDTH : distanceToRight}px`
+
 content.insertBefore(list, block)
 list.style.left = blockPosition.left + blockWidth + `px`
-list.style.top = `300px`
-
+list.style.top = BLOCK_TOP + `px`
 
 // отслеживание изменения местоположения розового блока
 const mutationObserver = new MutationObserver((mutations) => {
     mutations.forEach((mutation) => {
+        // на всякий случай, чтобы список не вылезал за пределы экрана справа
+        const distanceToRight = content.offsetWidth - content.offsetWidth * parseInt(mutation.target.style.left) / 100 - parseInt(mutation.target.style.width)
+        list.style.width = `${distanceToRight > LIST_WIDTH ? LIST_WIDTH : distanceToRight}px`
+
         list.style.top = mutation.target.style.top
         list.style.left = mutation.target.style.left
         list.style.transform = `translateX(${mutation.target.style.width})`
