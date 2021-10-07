62d2b79129185121d25e319028a3a0d6a1072b88
katylorny <katy_lorny@mail.ru>
2021-10-07 13:03:13 +0300

    fix

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
+const BLOCK_TOP = 300
+const LIST_WIDTH = 250
+const content = document.querySelector(`.content`)
+const list = document.createElement(`ul`)
+const block = document.querySelector(`.block`)
+const blockPosition = block.getBoundingClientRect()
+const blockWidth = block.clientWidth
+const distanceToRight = content.offsetWidth - block.getBoundingClientRect().left - block.clientWidth
+content.insertBefore(list, block)
+
+
+list.classList.add(`list`, `hidden`)
+
 // получение пунктов списка
 let posts = []
 fetch(`https://jsonplaceholder.typicode.com/posts/`)
-    .then(response => {
-        return response.json()
-    })
+    .then(response => response.json())
     .then(response => {
         posts = response.slice(0, 5)
         let postsTemplate = ``
@@ -24,36 +35,22 @@ fetch(`https://jsonplaceholder.typicode.com/posts/`)
 
     })
 
-const BLOCK_TOP = 300
-const LIST_WIDTH = 250
-
-// создание элемента списка
-const content = document.querySelector(`.content`)
-const list = document.createElement(`ul`)
-list.classList.add(`list`, `hidden`)
-
-
 // скрытие-показ списка по клику на блок
-const block = document.querySelector(`.block`)
 block.addEventListener(`click`, () => {
     list.classList.contains(`hidden`) ? list.classList.remove(`hidden`) : list.classList.add(`hidden`)
 })
 
-// расположение и ширина списка
-let blockPosition = block.getBoundingClientRect()
-let blockWidth = block.clientWidth
-
-const distanceToRight = content.offsetWidth - block.getBoundingClientRect().left - block.clientWidth
+// чтобы список не вылезал за пределы экрана справа (первоначально)
 list.style.width = `${distanceToRight > LIST_WIDTH ? LIST_WIDTH : distanceToRight}px`
 
-content.insertBefore(list, block)
+// первоначальное местоположение списка
 list.style.left = blockPosition.left + blockWidth + `px`
 list.style.top = BLOCK_TOP + `px`
 
-// отслеживание изменения местоположения розового блока
+// отслеживание изменения местоположения розового блока, вычисление местоположения и ширины списка
 const mutationObserver = new MutationObserver((mutations) => {
     mutations.forEach((mutation) => {
-        // на всякий случай, чтобы список не вылезал за пределы экрана справа
+        // чтобы список не вылезал за пределы экрана справа
         const distanceToRight = content.offsetWidth - content.offsetWidth * parseInt(mutation.target.style.left) / 100 - parseInt(mutation.target.style.width)
         list.style.width = `${distanceToRight > LIST_WIDTH ? LIST_WIDTH : distanceToRight}px`
 
